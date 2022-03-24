6c08cc130e7d35c2da2d957893c6cefac01497c5
JayaPrakash <kunchapu.jayaprakash@skillassure.com>
2022-03-24 15:18:35 +0530

    itemclick to view card

---
+++ app/src/main/java/com/example/checklist/consume/ViewTaskCard.kt
@@ -0,0 +1,11 @@
+package com.example.checklist.consume
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewTaskCard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_task_card)
+    }
+}
